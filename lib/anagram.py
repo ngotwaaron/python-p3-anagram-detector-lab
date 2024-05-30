3acd3670812715b1a2f99619dd546a9b9ca43153
ngotwaaron <ngotwaaron@gmail.com>
2024-05-30 09:28:08 +0300

    complete PYTHON -p3-anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        for word in words:
+            if sorted(word) == sorted(self.word) and word != self.word:
+                anagrams.append(word)
+        return anagrams
